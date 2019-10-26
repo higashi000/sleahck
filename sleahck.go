f7614768ea8ea6e0fbaf8f199b5ebf2ac87d7a26
higashi000 <higashi.ict@gmail.com>
2019-10-26 22:36:38 +0900

    [add] function for get channel history

--- sleahck.go
+++ sleahck.go
@@ -3,18 +3,47 @@ package main
 import (
     "github.com/nlopes/slack"
     "fmt"
+    "strconv"
+    "time"
+    "log"
     "os"
+//    "reflect"
     )
 
+type Message struct {
+  Text string `json:"text"`
+  User string `json:"user"`
+  Time string `json:"time"`
+}
+
+type Messages struct {
+  msg [50]Message
+}
+
 func main() {
+  channelID := ""
+
+  getHistory(channelID)
+}
+
+func getHistory(channelID string) *Messages {
+  var messages Messages
   api := slack.New(os.Getenv("SLACK_TOKEN"))
-  channelID := "channelID"
-  slackLog, err := api.GetChannelHistory(channelID, slack.HistoryParameters{"0", "0", 2, false, false})
+
+  slackLog, err := api.GetChannelHistory(channelID, slack.HistoryParameters{"0", "0", 50, false, false})
 
   if err != nil {
-    fmt.Println("error")
-    return
+    log.Println("error")
   }
 
-  fmt.Println(slackLog)
+  for i := 49; i >= 0; i-- {
+    messages.msg[i].Text = slackLog.Messages[i].Msg.Text
+    messages.msg[i].User = slackLog.Messages[i].Msg.User
+    tmpUnixTime, _ := strconv.Atoi(slackLog.Messages[i].Msg.Timestamp)
+    messages.msg[i].Time = (time.Unix(int64(tmpUnixTime), 0)).String()
+    fmt.Println(messages.msg[i])
+  }
+
+
+  return &messages
 }
